db25ec68b900782f39bffda049d5c7d0d18c3bc7
AEZ41 <slafolstudio@gmail.com>
2021-11-11 11:47:11 +0100

    Initialisation du projet

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import org.ejml.simple.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        SimpleMatrix m = new SimpleMatrix(3, 3);
+        System.out.println(m);
+    }
+}
