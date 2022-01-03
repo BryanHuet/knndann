a87516d8e1e2120ccfc8e1daaab09b5ba5451595
BryanHuet <huetb050@gmail.com>
2022-01-03 17:10:10 +0100

    ajout iris data et lecture des data

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
+import elements.DataIrisLecture;
 import jeigen.DenseMatrix;
 public class App {
     public static void main(String[] args){
+
         System.out.println("Hello here");
-        System.out.println("OK");
         DenseMatrix m = new DenseMatrix( new double[][]{{1,1,1},{1,-1,1},{1,1,1}} );
-        System.out.println(m);
+
+        System.out.println(DataIrisLecture.proceed("data/iris.data"));
+
     }
 }
---
+++ src/elements/DataIrisLecture.java
@@ -0,0 +1,34 @@
+package elements;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class DataIrisLecture {
+    public static HashSet<Iris> proceed(String path_to_file){
+        HashSet<Iris> iris_set = new HashSet<>();
+        BufferedReader reader;
+        try{
+            reader = new BufferedReader(new FileReader(path_to_file));
+            String line = reader.readLine();
+            while(line != null){
+                String[] attributes = line.split(",");
+                iris_set.add(new Iris(Float.parseFloat(attributes[0]),
+                        Float.parseFloat(attributes[1]),
+                        Float.parseFloat(attributes[2]),
+                        Float.parseFloat(attributes[3]),
+                        attributes[4]));
+                line = reader.readLine();
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return iris_set;
+    }
+}
