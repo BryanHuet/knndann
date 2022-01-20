ca104714678ef083cc9925b4c25b94503f516bfa
AEZ41 <slafolstudio@gmail.com>
2022-01-20 18:37:00 +0100

    Implémentation d'un parseur générique pour des données numériques

--- src/App.java
+++ src/App.java
@@ -1,9 +1,23 @@
-import org.ejml.simple.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import data.DataSet;
+import data.Point;
+import jeigen.DenseMatrix;
+import parser.*;
 
 public class App {
+    // java App "/data/iris.data" 3
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        SimpleMatrix m = new SimpleMatrix(3, 3);
-        System.out.println(m);
+        Parser numerique = new NumericalParser();
+        String chemin = "D:/Partage/Master/Projet/knndann/data/iris.data"; //"/data/iris.data";//args[0];
+        ArrayList<ArrayList<String>> data = numerique.parserFichier(chemin);
+        int[] separationDonnees = new int[]{0,50,100}; // 0 - n | n - size
+        DataSet bdd = new DataSet(data, separationDonnees, 4);
+        int k = 1;//Integer.parseInt(args[1]);
+        if(k < 1){
+            throw new ArithmeticException("k doit être un nombre supérieur à 1");
+        }
+        DANN dann = new DANN(bdd,1);
     }
 }
