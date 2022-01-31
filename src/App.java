c11d646b7e677467b292cd8197783ffc7a6d233d
BryanHuet <huetb050@gmail.com>
2022-01-31 12:48:19 +0100

    matrices B et W

--- src/App.java
+++ src/App.java
@@ -15,11 +15,25 @@ public class App {
         int k = 3;
 
         HashSet<Element> know_iris = DataIrisLecture.proceed("data/iris.data");
-        DenseMatrix query = new DenseMatrix( new double[][]{{3.6, 1.4, 5.5, 0.2}});
-        Element A = new Element(new DenseMatrix( new double[][]{{1, 1, 1, 1}}),0);
-        Element B = new Element(new DenseMatrix( new double[][]{{3, 3, 3, 3}}),0);
+        ArrayList<Element> iris = new ArrayList<>(know_iris);
 
-        System.out.println(DANN.distanceEuclidienne(A,B));
+        DenseMatrix query = new DenseMatrix( new double[][]{{3.6, 1.4, 5.5, 0.2}});
+        Element x0 = new Element(query,0);
+        Element A = new Element(new DenseMatrix( new double[][]{{0.3, 1.1, 1, 2.3}}),0);
+        Element B1 = new Element(new DenseMatrix( new double[][]{{6.1, 3, 3.2, 3}}),1);
+        Element C = new Element(new DenseMatrix( new double[][]{{1.2, 4.5, 3, 3}}),2);
+
+        System.out.println(DANN.B(iris,x0));
+        System.out.println(DANN.W(iris,x0));
+        DenseMatrix B = DANN.B(iris,x0);
+        DenseMatrix W = DANN.W(iris,x0);
+        DenseMatrix sigma = DenseMatrix.eye(4);
+        System.out.println(sigma);
+        sigma = W.mmul(
+                    W.mmul(B.mmul(W)).add(DenseMatrix.eye(4).mul(1))
+                ).mmul(W);
+        System.out.println(sigma);
+        //System.out.println(DANN.distanceEuclidienne(A,B));
 
 
 
--- src/classifier/DANN.java
+++ src/classifier/DANN.java
@@ -5,6 +5,9 @@ import jeigen.DenseMatrix;
 
 import java.util.*;
 
+import static jeigen.DenseMatrix.ones;
+import static jeigen.DenseMatrix.zeros;
+
 public class DANN {
 
     public static int NB_PARAMETERS = 4;
@@ -79,7 +82,7 @@ public class DANN {
      * @return double
      * Correspond à la formule pj du point (8)
      */
-    public double pj(Element x0, ArrayList<Element> elementNear, int j){
+    public static double pj(Element x0, ArrayList<Element> elementNear, int j){
         double numerator=0;
         double denominator=1;
         for(Element e1: elementNear){
@@ -89,6 +92,54 @@ public class DANN {
             denominator+=weights(e1,x0,max(x0,elementNear));
         }
         return numerator/denominator;
+    }
+
+    public static DenseMatrix moyenneXj(ArrayList<Element> elementNear,int j){
+        DenseMatrix moy = zeros(1,NB_PARAMETERS);
+        for (Element e: elementNear){
+            if (e.getClasse()==j){
+                moy.add(e.getVector());
+            }
+        }
+        return  moy.div(elementNear.size());
+    }
+    public static DenseMatrix moyenneX(ArrayList<Element> elementNear){
+        DenseMatrix moy = zeros(1,NB_PARAMETERS);
+        for (Element e: elementNear){
+                moy = moy.add(e.getVector());
+        }
+        return  moy.div(elementNear.size());
+    }
+
+    public static DenseMatrix B(ArrayList<Element> elementNear, Element x0){
+        DenseMatrix matrice = zeros(NB_PARAMETERS,NB_PARAMETERS);
+
+        for(int j=0; j<NB_PARAMETERS; j++){
+            double pj = pj(x0,elementNear,j);
+            DenseMatrix moy = moyenneX(elementNear).sub(moyenneXj(elementNear,j));
+
+            moy = moy.t().mmul(moy);
+            matrice = matrice.add(moy.mul(pj));
+        }
+        return matrice;
+    }
+
+    public static DenseMatrix W(ArrayList<Element> elementNear, Element x0){
+        DenseMatrix matrice = zeros(NB_PARAMETERS,NB_PARAMETERS);
+        double total_weigth=0;
+        for(int j=0;j<NB_PARAMETERS;j++){
+            for (Element e: elementNear) {
+                if(e.getClasse()==j){
+                    DenseMatrix current = e.getVector().sub(moyenneXj(elementNear,j));
+                    current = current.t().mmul(current);
+                    matrice=matrice.add(current.mul(weights(e,x0,max(x0,elementNear))));
+                }
+            }
+        }
+        for(Element e:elementNear){
+            total_weigth += weights(e,x0,max(x0,elementNear));
+        }
+        return matrice.div(total_weigth);
     }
 
 
