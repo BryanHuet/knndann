37625bd71a18c392688ef88b9437ee1b132e4b10
AEZ41 <slafolstudio@gmail.com>
2022-02-08 10:13:38 +0100

    Ajout du commentaire de classe

--- src/parser/ElementParser.java
+++ src/parser/ElementParser.java
@@ -7,7 +7,14 @@ import java.util.HashSet;
 import jeigen.DenseMatrix;
 
 public class ElementParser {
-    
+    /**
+     * Classe permettant de parcourir un fichier de type numérique, d'en extraire les données et de les préparer.
+     * Les données extraites sont ensuite stockées dans un vecteur de type DenseMatrix de la bibliothèque jeigen.
+     * Ce vecteur est enfin ajouté dans un objet de type Element qui représente un point de notre dataset.
+     * La classe d'un point est représenté par un entier naturel, ajouté également dans l'objet Element.
+     * @param path_to_file le chemin du fichier à parcourir.
+     * @return la liste des points de notre dataset.
+     */
     public static HashSet<Element> proceed(String path_to_file){
         HashSet<Element> iris_set = new HashSet<>();
         BufferedReader reader;
