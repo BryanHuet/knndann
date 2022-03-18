22544c3e245cfe78ada5e52c7a3a014c5c260f0c
AEZ41 <dannknn@gmail.com>
2022-03-18 11:32:10 +0100

    Mis à jour de la cross validation

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"classifier"},{"l":"elements"},{"l":"validator"}]
--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import elements.DataIrisLecture;
 import elements.NumericalParser;
 import elements.Element;
 import jeigen.DenseMatrix;
+import validator.BetaCV2;
 import validator.CrossValidation;
 
 import java.util.*;
@@ -26,29 +27,38 @@ public class App {
 
 
         String chemin = args[0]; 
-        int k=Integer.parseInt(args[1]);//3
+        int k = Integer.parseInt(args[1]);//3
         int nb_blocs = Integer.parseInt(args[2]);
 
 
         if(k < 1) {
             throw new ArithmeticException("k doit être un nombre supérieur à 1");
         }
-
+        if(nb_blocs < 2){
+            throw new IllegalArgumentException("Le nombre de blocs ne peut pas être < à 2");
+        }
 
         NumericalParser data_parse = new NumericalParser(chemin);
         HashSet<Element> data = data_parse.proceed();
 
-        DenseMatrix query = new DenseMatrix(new double[][]{{5.6, 3, 4.5, 1.5}});
-        Element x0 = new Element(query,0);
+        if(nb_blocs > data.size()/2){
+            throw new IllegalArgumentException("Le nombre de blocs ne peut pas être > à la longueur du dataset/2");
+        }
 
-        DANN dann = new DANN(data, k, 3, data_parse.getParametersNumber(), data_parse.getClassNumber());
+        BetaCV2 bc = new BetaCV2(data);
+        bc.crossValidation(nb_blocs, data_parse.getParametersNumber(), data_parse.getClassNumber(), new HashSet<>(Arrays.asList(1,3,15,25,50,100)));
+        
+        // DenseMatrix query = new DenseMatrix(new double[][]{{5.6, 3, 4.5, 1.5}});
+        // Element x0 = new Element(query,0);
+
+        // DANN dann = new DANN(data, k, 3, data_parse.getParametersNumber(), data_parse.getClassNumber());
 
 
-        System.out.println("Nombre de donnee "+data.size());
+        // System.out.println("Nombre de donnee "+data.size());
 
-        CrossValidation cv = new CrossValidation(data);
-        cv.crossValidation(nb_blocs,dann);
-        System.out.println(cv.getScore());
+        // CrossValidation cv = new CrossValidation(data);
+        // cv.crossValidation(nb_blocs,dann);
+        // System.out.println(cv.getScore());
 
 
     }
